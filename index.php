8a80eaf6173033f3cee1374d6281ee7ed6bca10d
Marek Kowalski <marek.kowalski@utilitia.pl>
2020-12-04 12:43:40 +0100

    Dodanie meta name viewport

--- index.php
+++ index.php
@@ -342,6 +342,7 @@ $_SESSION['s_linkStatus'] = "";
 <html lang="pl">
 	<head>
 		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>formularz</title>
 		<link rel="stylesheet" href="css/style.css">
 	</head>
