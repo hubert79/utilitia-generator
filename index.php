9e1ac41f27f380927ce5e26664b91a2f4c23a13d
Marek Kowalski <marek.kowalski@utilitia.pl>
2021-01-07 16:11:55 +0100

    CSS dla widoku wygenerowanego kodu deklaracji

--- index.php
+++ index.php
@@ -350,7 +350,7 @@
 	<head>
 		<meta charset="utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>formularz</title>
+		<title>Generator deklaracji dostępności – Utilitia</title>
 		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
